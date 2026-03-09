c5e7755258986794a3008e1f77131586d8d3df6c
Claudiano Goiana <resetprint@gmail.com>
2026-03-09 10:54:47 -0300

    09 03 2026

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Banner from "./components/Banner";
-import Container from "./components/Container";
-import videos from "./Json/db.json";
-import Category from "./components/Category";
-
-const categories = [
-  "Geografia", 
-  "Como fazer e usar", 
-  "Astronomia e Geografia", 
-  "Climatologia, Meteorologia, Vegetação", 
-  "Geologia e Hidrografia"
-];
-
-function filterCategory(Id) {
-  return videos.filter(video => video.category === categories[Id]);
-}
+import AppRoutes from "./routes";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Banner image="home" />
-      <Container>
-       
-      <Category category={categories[0]}>
-        {filterCategory(0).map((video) => <Card key={video.id} id={video.id} />)}
-      </Category>
-
-      <Category category={categories[1]}>
-        {filterCategory(1).map((video) => <Card key={video.id} id={video.id} />)}
-        </Category>
-
-        <Category category={categories[2]}>
-        {filterCategory(2).map((video) => <Card key={video.id} id={video.id} />)}
-      </Category>
-
-      <Category category={categories[3]}>
-        {filterCategory(3).map((video) => <Card key={video.id} id={video.id} />)}
-      </Category>
-
-      <Category category={categories[4]}>
-        {filterCategory(4).map((video) => <Card key={video.id} id={video.id} />)}
-      </Category>
-
-      </Container>
-      <Footer />
-    </>
+   <AppRoutes />
   );
 }
 
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,17 @@
+import video from "../../Json/db.json";
 import Styles from "./Category.module.css";
+
+export const categories = [
+  "Geografia", 
+  "Como fazer e usar", 
+  "Astronomia e Geografia", 
+  "Climatologia, Meteorologia, Vegetação", 
+  "Geologia e Hidrografia"      
+];
+
+export function filterCategory(Id) {
+  return video.filter(video => video.category === categories[Id]);
+}
 function Category({ category, children }) {
     return (
         <section className={Styles.category}>
